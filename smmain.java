391590ed7a2442bf1619447806bd7562dffb2340
sutapa4612 <39484717+sutapa4612@users.noreply.github.com>
2018-06-02 03:01:27 -0700

    Add files via upload

---
+++ smmain.java
@@ -0,0 +1,16 @@
+class sm
+{
+ static int F=100,C;
+static void display()
+{
+int C=5*(F-32)/9;
+System.out.println("Celcius is"+C);
+}
+}
+class smmain
+{
+public static void main(String args[])
+{
+sm.display();
+}
+}
